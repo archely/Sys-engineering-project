97f4efe0065d4187656e2ed6b65f4242d3a4795d
Arda Burak Atila <56447709+archely@users.noreply.github.com>
2020-10-04 18:12:38 +0300

    Update project.py

--- project.py
+++ project.py
@@ -21,19 +21,19 @@ def keyGenerator(): # private and public key
     return {"privateKey": privateKey, "publicKey": publicKey} 
 
 
-def encryption(publicKey, Encrypt): #encrypts /   public key
+def encrypt(publicKey, Encrypt): #encrypts /   public key
     ciphered = publicKey.encrypt(textToEncrypt, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
     return ciphered
 
-def decryption(privatKey, ciphered): #decrypts and encrypted  / private key
+def decrypt(privatKey, ciphered): #decrypts and encrypted  / private key
     noCipheredText = privatKey.decrypt(ciphered, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(), label=None))
     return Ciphered
 
-def symmetric_Encryption(symmetricKey, Encrypt): #encrypts message / symmetric key
+def symmetric_Encrypt(symmetricKey, Encrypt): #encrypts message / symmetric key
     cipher = Fernet(symmetricKey)
     return cipher.encrypt(Encrypt)
 
-def symmetric_Decryption(symmetricKey, Decrypt): #decrypts encrypted message / symmetric key
+def symmetric_Decrypt(symmetricKey, Decrypt): #decrypts encrypted message / symmetric key
     cipher = Fernet(symmetricKey)
     return cipher.decrypt(Decrypt)
 
@@ -41,18 +41,19 @@ def symmetric_Decryption(symmetricKey, Decrypt): #decrypts encrypted message / s
 bobkey = {}
 bobkey = keyGenerator() 
 
-# bob sends alice his public key
+
 Alicekey = {}
-Alicekey['publicKey_Bob'] = bobKey['publicKey'] 
-Alicekey['symmetricKey_Alice'] = Fernet.generate_key() #alice creates symmetric key
+Alicekey['publicKey'] = bobKey['publicKey'] # bob sends alice his public key
+Alicekey['symmetricKey'] = Fernet.generate_key() #alice creates symmetric key
 
 
-encryptionProcess = encryption(Alicekey['publicKey_Bob'], Alicekey['symmetricKey_Alice'])
+encryptKey = encryption(Alicekey['publicKey'], Alicekey['symmetricKey'])
 
 
-decryptionProcess = decryption(bobkey["privateKey"], encryptionProcess)
+dcryptKey = decrypt(bobkey["privateKey"], encryptKey)
 # encrypted symmetric key is sent to bob
-decryptedMsg = symmetric_Decryption(decryptionProcess, symmetric_Encryption(decryptionProcess, secretMsg))
+message_dcrypt = symmetric_Decrypt(dcryptKey, symmetric_Encrypt(dcryptKey, message))
 
+message =b"Hello"
 
-print("Message:" + {decryptedMsg.decode()} )
+print("Message:" + {message_dcrypt.decode()} )
